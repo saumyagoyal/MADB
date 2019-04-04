c9b86bb8a4f63562275497bfbb0a3c008c3d2b97
Saurabh Sharan <ss11485@nyu.edu>
2019-04-03 21:07:39 -0400

    Added LastFM Data

---
+++ src/data_gather_lastfm.py
@@ -0,0 +1,19 @@
+import json
+import requests
+
+data_points = []
+
+
+url= "http://ws.audioscrobbler.com/2.0/"
+params = {
+	"method": "chart.gettoptracks",
+	"api_key": "e86f0e6ebb5cf5776b4276d520ea541e",
+	"format": "json"
+}
+result = requests.get(url, params=params)
+print(result.status_code)
+print(result.text)
+data_points.append(json.loads(result.text))
+
+with open('lastfm_play.json', 'a+') as file:
+	file.write(json.dumps(data_points, indent=4, sort_keys=True))
